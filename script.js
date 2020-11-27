1aba3196ab8331e784463f13851ab5a624304bf8
Ekibunnel <dorian_live@live.fr>
2020-11-27 02:10:28 +0100

    Fix jitter

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function getTimeRemaining(endtime,nanodelta) {
-  const diff = Math.floor((window.performance.now() - Math.trunc(window.performance.now()/1000)*1000) *1000000);
-  const total = Date.parse(endtime)*1000000 - (new Date()*1000000 + diff);
+function getTimeRemaining(starttime, endtime) {
+  const diff = Math.floor(window.performance.now()*1000000);
+  const total = Date.parse(endtime)*1000000 - (starttime*1000000 + diff);
   const nanoseconds = Math.floor(total % 1000);
   const microseconds = Math.floor((total / 1000) % 1000);
   const milliseconds = Math.floor((total / 1000000) % 1000);
@@ -21,7 +21,7 @@ function getTimeRemaining(endtime,nanodelta) {
   };
 }
 
-function initializeClock(id, endtime) {
+function initializeClock(id, starttime, endtime) {
   const clock = document.getElementById(id);
   const daysSpan = clock.querySelector('.days');
   const hoursSpan = clock.querySelector('.hours');
@@ -32,17 +32,17 @@ function initializeClock(id, endtime) {
   const nanosecondsSpan = clock.querySelector('.nanoseconds');
 
   function updateClock() {
-    const t = getTimeRemaining(endtime,(window.performance.now() + 1.0));
+    const t = getTimeRemaining(starttime, endtime);
 
     daysSpan.innerHTML = t.days;
     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-    millisecondsSpan.innerHTML = ('0' + t.milliseconds).slice(-3);
-    microsecondsSpan.innerHTML = ('0' + t.microseconds).slice(-3);
-    nanosecondsSpan.innerHTML = ('0' + t.nanoseconds).slice(-3);
+    millisecondsSpan.innerHTML = ('00' + t.milliseconds).slice(-3);
+    microsecondsSpan.innerHTML = ('00' + t.microseconds).slice(-3);
+    nanosecondsSpan.innerHTML = ('00' + t.nanoseconds).slice(-3);
 
-    if (t.total <= 0) {
+    if (t.total <= 0 && timeinterval) {
       clearInterval(timeinterval);
     }
   }
@@ -51,5 +51,6 @@ function initializeClock(id, endtime) {
   const timeinterval = setInterval(updateClock, 1);
 }
 
+const starttime = new Date();
 const deadline = new Date(Date.parse("2020-12-10T01:59:59+0200"));
-initializeClock('clockdiv', deadline);
+initializeClock('clockdiv', starttime, deadline);
